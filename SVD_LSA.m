e1c05d4bfc90a7179510e3be2d5d6a99a39e1945
Ablomg <anton.blomgren14@gmail.com>
2023-02-06 17:34:44 +0100

    Skrev lite Steg inför PCA

--- SVD_LSA.m
+++ SVD_LSA.m
@@ -11,46 +11,52 @@ AAT = A*A';
 
 AAT = normalizeRows(AAT);
 
-% [U, S, V] = svd(A);
-% 
-% A = nolla(A);
-% 
-% VT = V';
-% 
-% num_topics = 4;
-% num_words = 10;
-% 
-% topic1 = topic(VT,1,num_words);
-% topic2 = topic(VT,2,num_words);
-% topic3 = topic(VT,3,num_words);
-% topic4 = topic(VT,4,num_words);
-% 
-% M = size(topic1);
-% if size(topic2) > M
-%     M = size(topic2);
-% end
-% if size(topic3) > M
-%     M = size(topic3);
-% end
-% if size(topic4) > M
-%     M = size(topic4);
-% end
-% num_words = M(1,1); 
-% 
-% TOPICS = zeros(  num_words,2*num_topics)  ;
-% [n,m] = size(topic1);
-% TOPICS(1:1:n,1:1:2) = topic1;
-% [n,m] = size(topic2);
-% TOPICS(1:1:n,3:1:4) = topic2;
-% [n,m] = size(topic3);
-% TOPICS(1:1:n,5:1:6) = topic3;
-% [n,m] = size(topic4);
-% TOPICS(1:1:n,7:1:8) = topic4;
+[U, S, V] = svd(A);
 
-% disp('-------------TOPIC 1-------------------TOPIC 2---------------------TOPIC 3-------------------TOPIC 4--------')
-% disp('-------ord-index---styrka----------ord-index---styrka-------ord-index---styrka--------ord-index---styrka--------')
-% %disp(           31     -0.25324         2508        0.122           50      0.45002           43      0.10303
-% disp(TOPICS)
+A = nolla(A);
+
+VT = V';
+
+num_topics = 4;
+num_words = 10;
+
+topic1 = topic(VT,1,num_words);
+topic2 = topic(VT,2,num_words);
+topic3 = topic(VT,3,num_words);
+topic4 = topic(VT,4,num_words);
+
+M = size(topic1);
+if size(topic2) > M
+    M = size(topic2);
+end
+if size(topic3) > M
+    M = size(topic3);
+end
+if size(topic4) > M
+    M = size(topic4);
+end
+num_words = M(1,1); 
+
+TOPICS = zeros(  num_words,2*num_topics)  ;
+[n,m] = size(topic1);
+TOPICS(1:1:n,1:1:2) = topic1;
+[n,m] = size(topic2);
+TOPICS(1:1:n,3:1:4) = topic2;
+[n,m] = size(topic3);
+TOPICS(1:1:n,5:1:6) = topic3;
+[n,m] = size(topic4);
+TOPICS(1:1:n,7:1:8) = topic4;
+disp('-------------TOPIC 1-------------------TOPIC 2---------------------TOPIC 3-------------------TOPIC 4--------')
+disp('-------ord-index---styrka----------ord-index---styrka-------ord-index---styrka--------ord-index---styrka--------')
+%disp(           31     -0.25324         2508        0.122           50      0.45002           43      0.10303
+disp(TOPICS)
+
+
+% STEG 1 (Skapa ny matris B=A-Aavg)
+
+
+% STEG 2 (Hitta SVD av B)
+
 
 
 function T = topic(VT,kolumn,num_words) 
